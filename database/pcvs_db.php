39ad6a95c203060171604dcd6bf3fbcbc731b920
ClintonTSR <70693551+ClintonTSR@users.noreply.github.com>
2021-11-04 15:00:47 +0800

    added for finding batch and vaccination with additional information

--- database/pcvs_db.php
+++ database/pcvs_db.php
@@ -31,3 +31,33 @@
 		$result = mysqli_query($db, $sql);
 		return $result ?: mysqli_error($db);
 	}
+
+	function find_batches_of_centre($centreName) {
+		$sql = "SELECT Batches.batchNo, expiryDate, vaccineName, quantityAvailable, quantityAdministered, 
+				COUNT(vaccinationID) AS quantityPending FROM Batches 
+				JOIN Vaccines ON Batches.vaccineID = Vaccines.vaccineID 
+				LEFT JOIN 
+				(SELECT batchNo, vaccinationID FROM Vaccinations WHERE status = 'pending') AS PendingVaccination 
+				ON Batches.batchNo = PendingVaccination.batchNo 
+				WHERE centreName = '$centreName'
+				GROUP BY Batches.batchNo";
+		
+		return queryAll($sql);
+	}
+
+	function find_batch($batchNo) {
+		$sql = "SELECT *, quantityAvailable - quantityOccupied AS 'quantityRemaining' FROM
+
+		(SELECT * FROM batches WHERE batchNo = '$batchNo') AS batch,
+	
+		(SELECT COUNT(*) AS 'quantityPending' FROM vaccinations WHERE batchNo = '$batchNo' AND status = 'pending') AS QuantityPending,
+	
+		(SELECT COUNT(*) AS 'quantityOccupied' FROM vaccinations WHERE batchNo = '$batchNo' AND status IN ('pending','confirmed','administered')) AS quantityOccupied;";
+
+		return queryAll($sql);
+	}
+
+	function find_vaccinations_of_batch($batchNo) {
+		$sql = "SELECT * FROM vaccinations WHERE batchNo = '$batchNo'";
+		return queryAll($sql);	
+	}

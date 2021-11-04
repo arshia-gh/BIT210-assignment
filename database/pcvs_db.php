5dcf258cb3ead559a98c430ae3d04d6966ba319d
ClintonTSR <70693551+ClintonTSR@users.noreply.github.com>
2021-11-04 17:39:02 +0800

    Update pcvs_db.php

--- database/pcvs_db.php
+++ database/pcvs_db.php
@@ -1,39 +1,48 @@
 <?php
 
-	require_once "db_connection.php";
+require_once "db_connection.php";
 
-	function find_vaccine($vaccine_id) {
-		$sql = "SELECT * FROM vaccines WHERE vaccineID = '$vaccine_id'";
-		return queryOne($sql);
-	}
+function find_vaccine($vaccine_id)
+{
+	$sql = "SELECT * FROM vaccines WHERE vaccineID = '$vaccine_id'";
+	return queryOne($sql);
+}
 
-	function get_all_vaccines() {
-		$sql = "SELECT * FROM vaccines";
-		return queryAll($sql);
-	}
+function get_all_vaccines()
+{
+	$sql = "SELECT * FROM vaccines";
+	return queryAll($sql);
+}
 
-	function query($sql) {
-		global $db;
-		return mysqli_query($db, $sql);
-	}
+function query($sql)
+{
+	global $db;
+	$result = mysqli_query($db, $sql);
+	if ($result === false)
+		throw new ErrorException(mysqli_error($db), mysqli_errno($db));
 
-	function queryOne($sql) {
-		return mysqli_fetch_assoc(query($sql));
-	}
+	return $result;
+}
 
-	function queryAll($sql) {
-		return mysqli_fetch_all(query($sql));
-	}
+function queryOne($sql)
+{
+	return mysqli_fetch_assoc(query($sql));
+}
 
-	function set_vaccination_status($vaccination_id, $status) {
-		global $db;
-		$sql = "UPDATE VACCINATIONS SET status = '$status' WHERE vaccinationID = $vaccination_id";
-		$result = mysqli_query($db, $sql);
-		return $result ?: mysqli_error($db);
-	}
+function queryAll($sql)
+{
+	return mysqli_fetch_all(query($sql), MYSQLI_ASSOC);
+}
 
-	function find_batches_of_centre($centreName) {
-		$sql = "SELECT Batches.batchNo, expiryDate, vaccineName, quantityAvailable, quantityAdministered, 
+function set_vaccination_status($vaccination_id, $status)
+{
+	$sql = "UPDATE VACCINATIONS SET status = '$status' WHERE vaccinationID = $vaccination_id";
+	return query($sql);
+}
+
+function find_batches_of_centre($centreName)
+{
+	$sql = "SELECT Batches.batchNo, expiryDate, vaccineName, quantityAvailable, quantityAdministered, 
 				COUNT(vaccinationID) AS quantityPending FROM Batches 
 				JOIN Vaccines ON Batches.vaccineID = Vaccines.vaccineID 
 				LEFT JOIN 
@@ -41,23 +50,47 @@
 				ON Batches.batchNo = PendingVaccination.batchNo 
 				WHERE centreName = '$centreName'
 				GROUP BY Batches.batchNo";
-		
-		return queryAll($sql);
-	}
 
-	function find_batch($batchNo) {
-		$sql = "SELECT *, quantityAvailable - quantityOccupied AS 'quantityRemaining' FROM
+	return queryAll($sql);
+}
 
+function find_batch($batchNo)
+{
+	$sql = "SELECT * FROM
 		(SELECT * FROM batches WHERE batchNo = '$batchNo') AS batch,
-	
-		(SELECT COUNT(*) AS 'quantityPending' FROM vaccinations WHERE batchNo = '$batchNo' AND status = 'pending') AS QuantityPending,
-	
-		(SELECT COUNT(*) AS 'quantityOccupied' FROM vaccinations WHERE batchNo = '$batchNo' AND status IN ('pending','confirmed','administered')) AS quantityOccupied;";
+		(SELECT COUNT(*) AS 'quantityPending' FROM vaccinations WHERE batchNo = '$batchNo' AND status = 'pending') AS quantityPending";
 
-		return queryAll($sql);
-	}
+	return queryAll($sql);
+}
 
-	function find_vaccinations_of_batch($batchNo) {
-		$sql = "SELECT * FROM vaccinations WHERE batchNo = '$batchNo'";
-		return queryAll($sql);	
+function find_vaccinations_of_batch($batchNo)
+{
+	$sql = "SELECT * FROM vaccinations WHERE batchNo = '$batchNo'";
+	return queryAll($sql);
+}
+
+// function confirm_appointment($vaccinationID) {
+
+// }
+
+function print_table($result)
+{
+	echo "<table class='table table-hover table-light'>";
+	foreach ($result as $index => $obj) {
+
+		if ($index === 0) {
+			echo "<tr class='table-secondary'>";
+			foreach ($obj as $key => $value) {
+				echo "<th>$key</th>";
+			}
+			echo "</tr>";
+		}
+
+		echo "<tr>";
+		foreach ($obj as $key => $value) {
+			echo "<td>$value<br /></td>";
+		}
+		echo "</tr>";
 	}
+	echo "</table>";
+}

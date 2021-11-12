4a6702661ee6366df20d0c0c90a7abdbfd3c31d4
Arshia Gholami <gholami.arshia@outlook.com>
2021-11-12 20:29:48 +0800

    updated app_metadata.inc.php

--- includes/app_metadata.inc.php
+++ includes/app_metadata.inc.php
@@ -1,5 +1,24 @@
 <?php
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+
+	session_start([
+		'cookie_lifetime' => 86400,
+		'cookie_httponly' => TRUE
+	]);
+
+	const PROJECT_DIR = "BIT210-assignment/";
+	define('PROJECT_PATH', "{$_SERVER['DOCUMENT_ROOT']}/" . PROJECT_DIR);
+	define('PROJECT_URL', "http://{$_SERVER['HTTP_HOST']}/". PROJECT_DIR);
 	const COPYRIGHT_TEXT = 'PCVS - copyright&copy; 2021';
 	const APP_SLOGAN = 'Private Covid-19 Vaccination Service ';
 	const APP_NAME = 'PCVS';
-	$constant = 'constant';
+
+	set_include_path(PROJECT_PATH);
+
+	function authenticate() {
+		if (isset($_SESSION['current_user'])) {
+			return $_SESSION['current_user'];
+		}
+		header("Location: " . PROJECT_URL . 'admin/index.php');
+		exit();
+	}

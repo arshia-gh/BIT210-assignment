9c104fdf281ea15d7cc48f748637836a687196d5
Arshia Gholami <gholami.arshia@outlook.com>
2021-11-16 16:25:06 +0800

    updated app_metadata.inc.php

--- includes/app_metadata.inc.php
+++ includes/app_metadata.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 	header('Cache-Control: no-store, no-cache, must-revalidate');
 
 	session_start([
@@ -7,7 +8,7 @@
 	]);
 
 	const PROJECT_DIR = "BIT210-assignment/";
-	define('PROJECT_PATH', "{$_SERVER['DOCUMENT_ROOT']}/" . PROJECT_DIR );
+	define('PROJECT_PATH', "{$_SERVER['DOCUMENT_ROOT']}/" . PROJECT_DIR);
 	define('PROJECT_URL', "http://{$_SERVER['HTTP_HOST']}/" . PROJECT_DIR);
 	const COPYRIGHT_TEXT = 'PCVS - copyright&copy; 2021';
 	const APP_SLOGAN = 'Private Covid-19 Vaccination Service ';
@@ -15,19 +16,33 @@
 
 	set_include_path(PROJECT_PATH);
 
+
 	/**
 	 * Gets the current user or redirect the user to the login page
+	 *
 	 * @return array|null the authenticated user. returns null on failure
 	 */
-	function authenticate(bool $redirectOnFailure = true): array|null
+	function authenticate(bool $redirectOnFailure = TRUE) : array|null
 	{
 		if (isset($_SESSION, $_SESSION['current_user'])) {
-			return $_SESSION['current_user'];
-		} else if ($redirectOnFailure) {
-			header('Location: ' . PROJECT_URL . 'users/login-form.php');
-			exit();
+			$user = $_SESSION['current_user'];
+			$dir = basename(dirname($_SERVER['PHP_SELF']));
+			if ($user['userType'] === 'administrator' && $dir == 'admin') {
+				return $user;
+			} else if ($user['userType'] === 'patient' && $dir == 'patient') {
+				return $user;
+			}
 		}
-		return null;
+		if ($redirectOnFailure) {
+			redirect_to_login_form();
+		}
+		return NULL;
+	}
+
+	function redirect_to_login_form() : void
+	{
+		header('Location: ' . PROJECT_URL . 'users/login-form.php');
+		exit();
 	}
 
 	// parse query string and initialize a constant
@@ -39,10 +54,10 @@
 	{
 		parse_str($_SERVER['QUERY_STRING'], $string_query_assoc);
 		if (isset($string_query_assoc[$key])) return PROJECT_URL . $string_query_assoc[$key];
-		foreach($rollbacks as $url) {
+		foreach ($rollbacks as $url) {
 			if (!is_null($url)) {
 				return $url;
 			}
 		}
-		return null;
+		return NULL;
 	}

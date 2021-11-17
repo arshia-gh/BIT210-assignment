477468fb103fa4ce784a79cd3d4fc551c93c3bb1
Arshia Gholami <gholami.arshia@outlook.com>
2021-11-17 22:58:25 +0800

    updated user_info.inc.php

--- includes/user_info.inc.php
+++ includes/user_info.inc.php
@@ -1,10 +1,10 @@
 <?php
-	$current_user = $_SESSION['current_user'] ?? NULL;
+	$found_user = $_SESSION['current_user'] ?? NULL;
 ?>
 	<aside class="border rounded bg-white p-3 mt-3">
 		<header class="d-flex justify-content-between align-items-end">
 			<h6 class="text-muted">User Information</h6>
-			<?php if (!is_null($current_user)): ?>
+			<?php if (!is_null($found_user)): ?>
 				<a class="btn btn-warning btn-sm" href="../users/logout.php">logout</a>
 			<?php else: ?>
 				<a class="btn btn-primary btn-sm" href="../users/login-form.php">login</a>
@@ -18,13 +18,13 @@
 			<footer class="col-9 col-lg-12 mt-3">
 				<ul class="list-group list-group-flush text-break text-center">
 					<?php
-						if (!is_null($current_user)) {
+						if (!is_null($found_user)) {
 
-							$special_info = $current_user['userType'] === 'administrator' ? 'staffID' : 'ICPassport';
+							$special_info = $found_user['userType'] === 'administrator' ? 'staffID' : 'ICPassport';
 							$columns_to_show = ['fullName', $special_info, 'email'];
 
 							foreach ($columns_to_show as $column) {
-								echo "<li class=\"list-group-item\">$current_user[$column]</li>";
+								echo "<li class=\"list-group-item\">$found_user[$column]</li>";
 							}
 						} else echo "<li class=\"list-group-item\">Not logged in</li>";
 					?>
@@ -32,4 +32,4 @@
 			</footer>
 		</figure>
 	</aside>
-<?php unset($current_user) ?>
+<?php unset($found_user) ?>
